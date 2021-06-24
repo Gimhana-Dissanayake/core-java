b013aa39373cca5d6388bc97c7456c63137bd1b8
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-24 09:09:39 +0530

    93.message passing

---
+++ section-5/92.polymorphism/Runner.java
@@ -0,0 +1,22 @@
+public class Runner {
+    // polymorhism is a Greek word, where poly means many and Morphism means forms
+    // or structures.
+
+    // If one thing is existing in more than one form then it is called as
+    // polymorphism.
+
+    // The main Adv of polymorphism is "Flexibility" to design applications.
+
+    // There are two types of polymorphism
+
+    // Static polymorphism
+    // If the polymorphism is existed at compilation time then it is called as
+    // Static polymorphism
+    // Ex: Method overloading
+
+    // Dynamic poly...
+    // If the pol.. is exited at runtime then that poly... is called as Dynamic poly
+    public static void main(String args[]) {
+
+    }
+}
