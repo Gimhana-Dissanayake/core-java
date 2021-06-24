6faf5ecb01f73137100f149ec8998a9d16a1ac60
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-24 08:35:09 +0530

    91.inheritance

---
+++ section-5/91.inheritance/Runner.java
@@ -0,0 +1,25 @@
+public class Runner {
+    public static void main(String args[]) {
+
+    }
+}
+
+class Employee {
+    String eid;
+    String ename;
+    float esal;
+
+    public void getEmpDetails() {
+
+    }
+
+}
+
+class Manager extends Employee {
+    // reuse variables and methods from employee
+}
+
+class Accountant extends Employee {
+    // reuse variables and methods from employee
+
+}
