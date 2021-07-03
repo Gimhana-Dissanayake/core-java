a8e5331a3d8f4d3a5cce74f10119b5c2a5e0c4e5
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-03 09:00:46 +0530

    95.access modifiers part-1

---
+++ section-5/95.access-modifiers-part-1/Runner.java
@@ -0,0 +1,16 @@
+public class Runner {
+
+    // Access modifiers
+    // There are two types of access modifiers in Java,
+
+    // 1.To define scope to the java programming elements
+    // public , protected, <default> and private
+
+    // To define some extra nature to the java programming elements, static, final,
+    // abstract, volatile, native , transient, synchronized, strictfp.
+
+    // <default> is the default scope
+
+    // package is collection of classes and interfaces
+
+}
