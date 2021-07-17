03d1407cd82278b6c240c3f27fe18d019471991c
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-17 08:32:14 +0530

    109.method syntax part-1

---
+++ section-5/109.method syntax part-1/Runner.java
@@ -0,0 +1,27 @@
+class Runner {
+    // Entities ------>classes
+    // Entities data ---> variables
+    // Entities behaviours ---> methods
+
+    // Method is a set of instructions, it can be used to represent a particular
+    // action of an entity.
+
+    // SYNTAX of a method
+    // [Access Modifiers] Return_Type methodName([Param_List]) [throws Exc_List]{
+    // ----------instructions--------
+    // }
+
+    // In java methods [Access Modifiers], [Param_List], [throws Exc_List] are
+    // optional.Return_Type is compulsary
+
+    // Access modifiers in Methods:
+    // ---> To define scope to the methods
+    // public, protected, <default> and private.
+
+    // From the above list of access modifiers all public, protected, <default>, and
+    // private are allowed in method syntax.
+
+    // To define some extra nature to the methods.
+    // static, final, abstract, native, volatile, transient, synchronized,
+    // strictfp,...
+}
