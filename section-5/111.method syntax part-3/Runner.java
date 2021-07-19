4ff83f9afa103b718d2132497a4e3655a76f43cc
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-20 00:02:31 +0530

    111.method syntax part-3

---
+++ section-5/111.method syntax part-3/Runner.java
@@ -0,0 +1,29 @@
+public class Runner {
+
+    // [Access Modifiers] ReturnType methodName([param_List]) [throws exe_list]{
+    // ....insturctions
+    // }
+
+    // Method return type is representing which type of data we are going to return
+    // from the present method.
+
+    // Java methods are able to allow all primitive data type like byte, short, int,
+    // long, float, double, char, boolean, all user defined data types like classes,
+    // abstract classes, arrays, interfaces, ...and void
+
+    // Note: "void" return type is representing no data is returing from methods
+
+    // Where methodName is an identifier, it can be used to recognize the methods
+    // individually.
+
+    // Where the purpose of parameters List is to take some input data inorder to
+    // perform an action which is represented by the present method.
+
+    // Where method parameters may be all primitive data types like byte, short,
+    // int, long, float, double, char, boolean, all user defined data types like all
+    // classes, abstract classes, arrays, interfaces
+
+    // Where throws is a java keyword, it can be used to bypass the generated
+    // exception from present method to the caller method.
+
+}
