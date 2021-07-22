6ea57c7e24baa5a54c2df8ad4642b09aa756b7e2
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-22 12:40:33 +0530

    119.OOPS: procedure to create objects part =-2

---
+++ section-5/119.OOPS. procedure to create objects part-2/Runner.java
@@ -0,0 +1,12 @@
+class Runner {
+
+    // When we write java command and press enter, the operating system will execute
+    // java command present in C:/java/jdk/bin/
+    // when we execute java command java software will be activated
+    // Then the class is loaded to method area(the byte code is loaded to the method
+    // area). byte codes are loaded only to method area
+    // Then jvm will execute main method
+
+    // WATCH THIS VIDEO AGAIN
+
+}
