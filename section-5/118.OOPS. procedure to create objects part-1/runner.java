22e7bb1f3d33f88cc3aa3cdfc6dc80f52c6d41b7
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-22 08:24:38 +0530

    118. oops. procedure to create objects part-1

---
+++ section-5/118.OOPS. procedure to create objects part-1/runner.java
@@ -0,0 +1,13 @@
+public class runner {
+
+    // The main intention to create objects in Java is,
+
+    // 1. Java is an object oriented programming language, in java applications
+    // everything is associated with objects only.
+    // 2. To store entities data in Java applications temporarily.
+    // 3. To access members of a particular class.
+
+    // Syntax
+    // Class_Name ref_var = new Class_Name([Param_Values]);
+
+}
