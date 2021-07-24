0aa2223497533e55cdb00dd1eb91d64148a5e0f3
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-24 14:03:32 +0530

    127. OOPS: object vs instance

---
+++ section-5/127. OOPS. object vs instance/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+
+    // Q. what is the difference between object and instance
+    // Ans: Object is a memory block to store data. Instance is the copy of data or
+    // layer of data existed in an object at a particular point of time
+
+    // In a single object, we are able to get no of instances, but, all the
+    // instances are existed in overridden manner.
+
+}
