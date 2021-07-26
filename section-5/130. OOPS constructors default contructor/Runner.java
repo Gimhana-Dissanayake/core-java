c75eba8c7c99b9ee446121b6328486bcd308e63a
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-26 08:04:02 +0530

    130. OOPS. contructors : default constructor

---
+++ section-5/130. OOPS constructors default contructor/Runner.java
@@ -0,0 +1,27 @@
+public class Runner {
+    // There are two types of contructors
+    // 1. default contructors(0-arg contructor)
+    // 2. user defined contructor(This has two types 1. 0-arg constructor
+    // 2.Parameterized constructor)
+
+    // In java applications, if we have not provided any contructor explicitly in a
+    // class then compiler will add a 0-arg contructor internally, where the
+    // compiler provided contructor is called as "Default-constructor". If we
+    // provide any constructor to the class explicitly then compiler will not add
+    // default contructor.
+
+    // NOTE: The scope of the default constructor is same as the respective class
+    // scope. If the class scope is public then the default contructor will be
+    // provided by the compiler with public scope only
+
+    // NOTE: In java applications, compiler will provide 0-arg constructor as
+    // default constructor, compiler will not provide parameterized constructor as
+    // default contructor.
+
+    // NOTE: In java applications, by default , all default contructors are 0-arg
+    // contructors only, but, all 0-arg contructors need not be default contructor,
+    // the 0-arg contructors which are provided by compiler are default contructors
+    // and the 0-arg contructors which are provided by the developers come under
+    // user defined constructors.
+
+}
