a7e0b2c49891aeb1d9683fdbbe56f657e0b28d1b
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-29 07:47:10 +0530

    formatting

--- 268. Introduction part-1/Runner.java
+++ 268. Introduction part-1/Runner.java
@@ -3,4 +3,10 @@ public class Runner {
     // Collection : collection is an object , it able to represent or store a group
     // of other objects.
 
+    // NOTE: In java ,collection objects are able to store only objects reference
+    // values, collection objects are not going to store primitive data directly
+
+    // If we want to store primitive data in collection object then we have to use
+    // the fallowing procedure
+
 }
