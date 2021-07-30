0577042ea7d8ae5e02a2015da3198ed87e28c358
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-30 08:04:57 +0530

    134. OOPS. constructors user defined contructors

---
+++ section-5/134. OOPS. constructors user defined constructors part-4/Runner.java
@@ -0,0 +1,9 @@
+class Runner {
+    // local variable's data will be stored in stack memory
+    // Instance variable's data will be stored in heap memory
+
+    // in general objects are created in heap memory.so these variables have access
+    // though out the class.
+    // stack memory is only available throught out the method
+
+}
