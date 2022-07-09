876cba40eca62ab852f6a014a01acdcd64ddf310
GimhanaDissanayake <gimhanadev@gmail.com>
2022-07-09 13:44:35 +0530

    195

---
+++ section-5/195. OOPS. Relationships in java Associations Composition vs Aggregation/Runner.java
@@ -0,0 +1,16 @@
+//In Java applications, Associations are represented in the form of the following two types
+
+// 1. Composition
+// 2. Aggregation
+
+// Q. What are the differences between composition and aggregation
+
+// 1. Composition is representing strong association between entities. Aggregation is representing weak association between entities
+
+//2. In case of composition, if we close container then contained must be closed automatically. In case of Aggregation, even if we close container then contained may be existed.
+
+// 3. In case of composition, contained lifetime is depending on container lifetime. In case of Aggregation, contained lifetime is not depending on container lifetime.
+
+public class Runner {
+
+}
