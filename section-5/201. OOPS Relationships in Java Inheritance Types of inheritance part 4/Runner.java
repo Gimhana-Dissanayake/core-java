6a3a6bceb8ad01ad8b9f4d751d795cc43a00fa67
GimhanaDissanayake <gimhanadev@gmail.com>
2022-09-25 23:12:32 +0530

    204

---
+++ section-5/201. OOPS Relationships in Java Inheritance Types of inheritance part 4/Runner.java
@@ -0,0 +1,41 @@
+
+// 4. Hierarchical Inheritance
+// It is the combination of single inheritances in a particular Arch.
+// Java does support this inheritance.
+
+class A {
+
+}
+
+class B extends A {
+
+}
+
+class C extends A {
+
+}
+
+class D extends B {
+
+}
+
+class E extends B {
+
+}
+
+class F extends C {
+
+}
+
+class G extends F {
+
+}
+
+// 5. Hybrid Inheritance
+// It is the combination of single inheritance and multiple inheritance.
+
+// java does not support this inheritance.
+
+public class Runner {
+
+}
