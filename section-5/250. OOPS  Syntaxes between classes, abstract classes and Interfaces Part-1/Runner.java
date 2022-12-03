e12d4f851006a34e4264ca842b130c7a6418fd71
GimhanaDissanayake <gimhanadev@gmail.com>
2022-12-03 15:36:31 +0530

    250

---
+++ section-5/250. OOPS  Syntaxes between classes, abstract classes and Interfaces Part-1/Runner.java
@@ -0,0 +1,22 @@
+
+// Q. Find the valid syntaxs between classes, abstract classes and interfaces from the following list?
+
+// 1. class extends class ---> valid 
+// 2. class extends abstract class ---> valid
+// 3. class extends interface ---> invalid
+// 4. class extends class, class ---> invalid
+// 5. class extends class, abstract class ---> invalid
+// 6. class extends abstract class, abstract class -->-> invalid
+// 7. class implements interface ---> valid
+// 8. class implements interface, interface ---> valid
+// 9. class implements class ---> invalid
+// 10. class extends class implements interface ---> valid
+// 11. class extends class implements interface, interface ---> valid
+// 12. class extends abstract class implements interface --->valid
+// 13. class extends abstract class implments interface, interface ---> valid
+// 14. class implements interface extends class ---> invalid
+// 15. class implements interface extends abstract class ---> invalid
+
+public class Runner {
+
+}
