6af9cbbfa4ea17c11e04d53a99476318a579ee09
Gimhana-Dissanayake <gimhanadev@gmail.com>
2023-03-29 07:41:40 +0530

    179

--- section-14/179. Deque Methods Part-2/Runner.java
+++ section-14/179. Deque Methods Part-2/Runner.java
@@ -1,6 +1,10 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+// Q) What is the diference between addLast() and offerLast()?
+
+// Ans:
+// Both the methods are used to add the provided element as last element at Tail side, if we access addLast() method to the bounded queues over its max capacity then JVM will raise an exception, but, if we access offerLast() method over its max capacity in bounded queues then JVM will not raise any excpetion.
 public class Runner {
 
   public static void main(String[] args) {
