896f88ecb12b1b66f9ee255a3be9c578f6f0549c
Gimhana-Dissanayake <gimhanadev@gmail.com>
2023-04-02 23:28:51 +0530

    191. Map interface Overview

---
+++ section-14/191. Map interface Overview/Runner.java
@@ -0,0 +1,25 @@
+// Map:
+
+// It was introduced by Java in its JDK1.2 version.
+// Map is not child interface to Collection, Map is one of the root interface in Collection Framework.
+// It is able to represent all the element in the form of key-value pairs.
+
+// Ex: To represent technology directory, where data like phone number and customer name are represented in the form of key-value pairs there we will use Maps.
+
+// Ex: to represent Students attendance register we will use Maps, because, in students attendence register data like student roll number and student name represent  in the form of key-value pairs.
+
+// In Maps, both keys and values are objects, key and values are not primitives data types, atleast, wrapper classes objects.
+// It key-value pairs, keys must be unique, they must not allow duplicates,but, values may be duplicated.
+// In maps, by default, both insertion order and sorting order are not followed.
+// NOTE: Following insertion order or sorting order is varied from one implementation class to another implementation class.
+// EX: HashMap is not following both insertion order and sorting order.
+// EX: LinkedHashMap is able to follow insertion order.
+// EX: SortedMap, NavigableMap, TreeMap are able to follow sorting order.
+// In Maps, bydefault, Heterogenous elements are allowed.
+// Note: SortedMap, NaviagleMap and TreeMap are able to allow only Homogenous elements at keys side.
+// In maps, by default, keys are able to allow only one null element, because, keys are not allowing duplicates, but values are able to allow any no of null elements.
+// Note: SortedMap, NavigableMap, TreeMap are not allowing even single null element at keys
+public class Runner {
+
+  public static void main(String[] args) {}
+}
